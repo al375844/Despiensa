c9cc48f47e3da1ab57431a72f62bacb3e56a3c8c
unknown <al375844@uji.es>
2021-03-17 12:26:52 +0100

    mongoDB: addItemToDespensa query añadida y provada

--- mongoDB/mongoQueries/addItemToDespensa.js
+++ mongoDB/mongoQueries/addItemToDespensa.js
@@ -1,5 +1,7 @@
-db.users.updateOne({nombreUsuario: "Paco"}, {
-        $push: {
+db.users.updateOne(
+    {nombreUsuario: "Paco"}, 
+    {
+        $addToSet: {
             despensa: {
                 "alimento": ObjectId("6051e111b591fc0a91eaf1a6"),
                 "cantidad": 4
