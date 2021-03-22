64249cb8bdea636f5e6a024afea6d796fd3308fa
unknown <al375844@uji.es>
2021-03-22 12:35:45 +0100

    backend: usersRoutes del sprint 1 completadas

--- backend/src/controller/usersController.js
+++ backend/src/controller/usersController.js
@@ -9,14 +9,12 @@ usersController.newUser = async(req, res, next) => {
     console.log("🚀 ~ file: usersController.js ~ line 9 ~ usersController.nuevoUsuario=async ~ correo", correo)
     const password = req.params.password;
     console.log("🚀 ~ file: usersController.js ~ line 11 ~ usersController.nuevoUsuario=async ~ password", password)
-    const fechaNacimiento = req.params.fechaNacimiento;
-    console.log("🚀 ~ file: usersController.js ~ line 13 ~ usersController.nuevoUsuario=async ~ fechaNacimiento", fechaNacimiento)
     const planName = req.params.planName;
-    console.log("🚀 ~ file: usersController.js ~ line 15 ~ usersController.nuevoUsuario=async ~ planName", planName)
+    console.log("🚀 ~ file: usersController.js ~ line 13 ~ usersController.nuevoUsuario=async ~ planName", planName)
     const alergias = req.params.alergias;
-    console.log("🚀 ~ file: usersController.js ~ line 17 ~ usersController.nuevoUsuario=async ~ alergias", alergias)
+    console.log("🚀 ~ file: usersController.js ~ line 15 ~ usersController.nuevoUsuario=async ~ alergias", alergias)
     const intolerancias = req.params.intolerancias;
-    console.log("🚀 ~ file: usersController.js ~ line 19 ~ usersController.nuevoUsuario=async ~ intolerancias", intolerancias)
+    console.log("🚀 ~ file: usersController.js ~ line 17 ~ usersController.nuevoUsuario=async ~ intolerancias", intolerancias)
     res.send({"id": 0, "error": "Ya Existente."});
 }
 
--- backend/src/routes/usersRoutes.js
+++ backend/src/routes/usersRoutes.js
@@ -3,9 +3,23 @@ const usersController = require('../controller/usersController');
 
 const router = express.Router();
 
-router.post('/newUser/:nombre/:apellidos/:correo/:password/:fechaNacimiento/:alergias/:intolerancias/:planName', usersController.newUser);
+router.post('/newUser/:nombre/:apellidos/:correo/:password/:alergias/:intolerancias/:planName', usersController.newUser);
 
-router.post('/userModification/:nombre/:apellidos/:correo', usersController.modifyUser);
+router.put('/modifyUser/:nombre/:apellidos/:correo', usersController.modifyUser);
+
+router.put('/newPassword/:oldPassword/:newPassword', usersController.newPassword);
+
+router.put('/deleteUser/:correo', usersController.deleteUser);
+
+router.put('/getUser/:correo', usersController.getUser);
+
+router.put('/newPassword/:nombre/:apellidos/:fechaNacimiento/:alergias/:intolerancias', usersController.newProfile);
+
+router.put('/newPassword/:nombre/:apellidos/:fechaNacimiento/:alergias/:intolerancias', usersController.modifyProfile);
+
+router.put('/newPassword/:nombreyApellidos', usersController.deleteProfile);
+
+router.put('/newPassword/:nombreyApellidos', usersController.getProfile);
 
 
 module.exports = router;
