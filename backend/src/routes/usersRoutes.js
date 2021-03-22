a1dd8f33f534879a6deecd2158593ce6c757a3ad
unknown <al375844@uji.es>
2021-03-22 12:47:09 +0100

    backend: ligeras modificaciones en usersRoutes.

--- backend/src/routes/usersRoutes.js
+++ backend/src/routes/usersRoutes.js
@@ -3,23 +3,23 @@ const usersController = require('../controller/usersController');
 
 const router = express.Router();
 
-router.post('/newUser/:nombre/:apellidos/:correo/:password/:alergias/:intolerancias/:planName', usersController.newUser);
+router.post('/newUser/:usuario/:nombre/:apellidos/:correo/:password/:alergias/:intolerancias/:planName', usersController.newUser);
 
-router.put('/modifyUser/:nombre/:apellidos/:correo', usersController.modifyUser);
+router.put('/modifyUser/:usuario/:nombre/:apellidos/:correo', usersController.modifyUser);
 
-router.put('/newPassword/:oldPassword/:newPassword', usersController.newPassword);
+router.put('/newPassword/:usuario/:oldPassword/:newPassword', usersController.newPassword);
 
-router.put('/deleteUser/:correo', usersController.deleteUser);
+router.put('/deleteUser/:usuario', usersController.deleteUser);
 
-router.put('/getUser/:correo', usersController.getUser);
+router.put('/getUser/:usuario', usersController.getUser);
 
-router.put('/newPassword/:nombre/:apellidos/:fechaNacimiento/:alergias/:intolerancias', usersController.newProfile);
+router.put('/newPassword/:usuario/:nombre/:apellidos/:fechaNacimiento/:alergias/:intolerancias', usersController.newProfile);
 
-router.put('/newPassword/:nombre/:apellidos/:fechaNacimiento/:alergias/:intolerancias', usersController.modifyProfile);
+router.put('/newPassword/:usuario/:nombre/:apellidos/:fechaNacimiento/:alergias/:intolerancias', usersController.modifyProfile);
 
-router.put('/newPassword/:nombreyApellidos', usersController.deleteProfile);
+router.put('/newPassword/:usuario/:nombreyApellidos', usersController.deleteProfile);
 
-router.put('/newPassword/:nombreyApellidos', usersController.getProfile);
+router.put('/newPassword/:usuario/:nombreyApellidos', usersController.getProfile);
 
 
 module.exports = router;
