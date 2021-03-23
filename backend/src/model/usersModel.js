8efd28925dc0dfda93a57bf65202b0d606244994
unknown <al375844@uji.es>
2021-03-23 11:16:49 +0100

    backend: primera aproximación a usersModel.

--- backend/src/model/usersModel.js
+++ backend/src/model/usersModel.js
@@ -1,23 +1,47 @@
 const getDB = require('../utils/database').getDB;
 
 class Users {
-    constructor (usuario, nombreUsuario, apellidosUsuario, correo, password, puntuacionUsuario, n_recetas, plan, despensa, perfiles, listas, recetas) {
+    constructor (usuario, nombreUsuario, apellidosUsuario, correo, password, plan) {
         this.usuario = usuario;
         this.nombreUsuario = nombreUsuario;
         this.apellidosUsuario = apellidosUsuario;
         this.correo = correo;
         this.password = password;
-        this.puntuacionUsuario = puntuacionUsuario;
-        this.n_recetas = n_recetas;
-        this.plan = plan;
-        this.despensa = despensa;
-        this.perfiles = perfiles;
-        this.listas = listas;
-        this.recetas = recetas;
+        this.plan = plan; //Nos llega el nombre del plan.
+        this.alergias = alergias;
+        this.intolerancias = intolerancias;
     }
 
-    save(){
-
+    newUser(){
+        const db = getDB();
+        const planId = db.collection('plans')
+            .find({"nombre": this.plan})
+            .project({
+                "nombre": 0,
+                "descripcion": 0,
+                "precio": 0
+            })
+            .then(result => {
+                console.log(result);
+            })
+            .catch(err => {
+                console.log(err);
+            });
+        db.collection('users2')
+            .insertOne({
+                "usuario" : this.usuario,
+                "nombreUsuario" : this.nombreUsuario,
+                "apellidosUsuario" : this.apellidosUsuario,
+                "correo": this.correo,
+                "password": this.password,
+                "plan" : planId
+            })
+            .then(result => {
+                console.log(result);
+            })
+            .catch(err => {
+                console.log(err);
+            });
     }
 }
 
