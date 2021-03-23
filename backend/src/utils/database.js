b851304b0d083240f057e84d8ea5cc949417bc16
unknown <al375844@uji.es>
2021-03-23 10:52:14 +0100

    backend: database.js modificado, nuevo método getDB añadido, este método devuelve la conexión a la base de datos en caso de que se haya realizado correctamente.

--- backend/src/utils/database.js
+++ backend/src/utils/database.js
@@ -1,17 +1,29 @@
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 
+let _db;
+
 const mongoConnect = (callback) => {
     MongoClient.connect(
         'mongodb+srv://node-backend:node-backend@despiensa.wtoei.mongodb.net/despiensa?retryWrites=true&w=majority'
     )
         .then( client => {
             console.log('Conectado');
-            callback(client);
+            _db = client.db(); //Proporciona acceso a la base de datos identificada en la uri, en este caso Despiensa.
+            callback();
         })
         .catch( err => {
             console.log(err);
+            throw err;
         });
 };
 
-module.exports = mongoConnect;
+const getDB = () => {
+    if (_db) {
+        return _db;
+    }
+    throw 'No database found!';
+}
+
+exports.mongoConnect = mongoConnect;
+exports.getDB = getDB;
