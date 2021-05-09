075b2fcf610edf52fe4d296878e294cb6e2823ba
Nestor Lopez Olaria <al375844@uji.es>
2021-05-09 23:56:21 +0200

    backend: modifyPlan en planModel terminado sin provar

--- backend/src/model/planModel.js
+++ backend/src/model/planModel.js
@@ -15,7 +15,43 @@ class Plan {
             .toArray();
 
     }
-    
+
+    async modifyPlan(usuario, password){
+
+        const db = getDB();
+
+        const cursor = await db.collection('plans')
+            .find({"nombre": this.nombre});
+        const existe = await cursor.hasNext();
+        if (! existe){
+            throw [2,'El plan indicado no existe.'];
+        }
+        const plan = await cursor.next();
+
+        await db.collection('users')
+            .updateOne(
+                {
+                    usuario: usuario,
+                    password: password
+                },
+                {
+                    $set: {
+                        "plan": plan._id
+                    }
+                }
+            )
+            .then(result => {
+                if(result.modifiedCount == 0){
+                    console.log("Lanzamos el error");
+                    throw [10, 'El usuario no existe o la contraseña vieja no se corresponde con la indicada.'];
+                }
+            });
+
+        return db.collection('users')
+            .findOne({usuario: this.usuario});
+
+    }
+
 }
 
 module.exports = Plan;
