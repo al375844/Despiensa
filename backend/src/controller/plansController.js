52f64fa88775ead8ccd2ac860a3e685d06fdfc9c
Nestor Lopez Olaria <al375844@uji.es>
2021-05-09 23:34:55 +0200

    backend: metodo getPlans terminado pero no probado.

--- backend/src/controller/plansController.js
+++ backend/src/controller/plansController.js
@@ -3,15 +3,16 @@ const plansController = {};
 const Plan = require('../model/planModel');
 
 plansController.getPlans = (req, res, next) => {
-    res.send(
-        {
-            "_id": "0",
-            "error": {
-                "type" : 0,
-                "message" : "No implementada"
-            }
-        }
-    );
+    
+    let plan = new Plan("0");
+
+    plan.getPlans()
+        .then(result => {
+            res.send(result);
+        })
+        .catch(err => {
+            res.send(err);
+        });
 }
 
 plansController.modifyPlan = (req, res, next) => {
--- backend/src/model/planModel.js
+++ backend/src/model/planModel.js
@@ -2,6 +2,20 @@ const getDB = require('../utils/database').getDB;
 
 class Plan {
 
+    constructor(nombre){
+        this.nombre = nombre;
+    }
+
+    async getPlans(){
+
+        const db = getDB();
+
+        return db.collection('plans')
+            .find()
+            .toArray();
+
+    }
+    
 }
 
 module.exports = Plan;
