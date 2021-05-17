d12f445d174aa8de76c8630846b25e043d1a1a93
nestor.lopez <nestor.lopez@easygoband.com>
2021-05-17 13:25:10 +0200

    backend: rutas de listas arregladas.

--- backend/src/routes/listsRoutes.js
+++ backend/src/routes/listsRoutes.js
@@ -4,14 +4,14 @@ const listsController = require('../controller/listsController');
 const router = express.Router();
 
 //'lists'
-router.put('/newList/:usuario/:nombrePerfil/:nombreLista', listsController.newList);
+router.put('/newList/:usuario/:nombreLista', listsController.newList);
 
-router.put('/deleteList/:usuario/:nombrePerfil/:nombreLista', listsController.deleteList);
+router.put('/deleteList/:usuario/:nombreLista', listsController.deleteList);
 
-router.put('/updateList/:usuario/:nombrePerfil/:nombreLista', listsController.updateList);
+router.put('/updateList/:usuario/:nombreLista', listsController.updateList);
 
-router.put('/addFood/:usuario/:nombrePerfil/:nombreLista', listsController.addFood);
+router.put('/addFood/:usuario/:nombreLista', listsController.addFood);
 
-router.put('/deleteFood/:usuario/:nombrePerfil/:nombreLista', listsController.deleteFood);
+router.put('/deleteFood/:usuario/:nombreLista', listsController.deleteFood);
 
-router.put('/buyFood/:usuario/:nombrePerfil/:nombreLista', listsController.buyFood)
+router.put('/buyFood/:usuario/:nombreLista', listsController.buyFood)
