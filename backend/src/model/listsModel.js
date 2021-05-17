764e73a403080b055cf8c9092d33ecaf129bed4e
nestor.lopez <nestor.lopez@easygoband.com>
2021-05-17 22:45:18 +0200

    backend: método newList del modelo listModel terminado sin probar.

--- backend/src/model/listsModel.js
+++ backend/src/model/listsModel.js
@@ -1,6 +1,44 @@
 const getDB = require('../utils/database').getDB;
 
 class List {
+    constructor(nombreLista){
+        this.nombreLista = nombreLista;
+    }
+
+    newList(usuario){
+
+        const db = getDB();
+
+        const cursor = await db.collection('food')
+            .find({"nombre": "nada"});
+        const nada = await cursor.next();
+
+        await db.collection('users')
+            .updateOne(
+                {
+                    "usuario": usuario
+                },
+                {
+                    $push:
+                    {
+                        listas: 
+                        {
+                            $each: [
+                                {
+                                    "nombreLista": this.nombreLista,
+                                    "alimentos": [
+                                        {
+                                            "alimento": nada._id,
+                                            "cantidad": 0
+                                        }
+                                    ]
+                                }
+                            ]
+                        }  
+                    }
+                }
+            );
+    }
 
 }
 
